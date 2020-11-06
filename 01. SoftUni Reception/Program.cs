1755d07707ca6e1ee85b07f4a5cd8ab28cc5dd71
IvanHrIvanov <vinko96@abv.bg>
2020-11-06 02:01:01 +0200

    Add project files.

---
+++ 01. SoftUni Reception/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empoyesOne = int.Parse(Console.ReadLine());
+            int empoyesTwo = int.Parse(Console.ReadLine());
+            int empoyesThree = int.Parse(Console.ReadLine());
+
+            int totalPeople = int.Parse(Console.ReadLine());
+
+
+            int allAnswer = empoyesOne + empoyesTwo + empoyesThree;
+            int timeNeedet = 0;
+
+            while (totalPeople > 0)
+            {
+                timeNeedet++;
+                totalPeople -= allAnswer;
+
+                if (timeNeedet % 4 == 0)
+                {
+                    timeNeedet++;
+                }
+
+            }
+            Console.WriteLine($"Time needed: {timeNeedet}h.");
+        }
+    }
+}
---
+++ 02. Array Modifier/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _02._Array_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            string[] comand = Console.ReadLine().Split(" ");
+
+            while (comand[0] != "end")
+            {
+                if (comand[0] == "swap" || comand[0] == "multiply")
+                {
+
+                    int indexOne = int.Parse(comand[1]);
+                    int indexTwo = int.Parse(comand[2]);
+
+                    int firstNumber = array[indexOne];
+                    int secondNumber = array[indexTwo];
+
+                    if (comand[0] == "swap")
+                    {
+
+                        array[indexOne] = secondNumber;
+                        array[indexTwo] = firstNumber;
+
+                    }
+                    else if (comand[0] == "multiply")
+                    {
+
+                        array[indexOne] = array[indexOne] * array[indexTwo];
+
+                    }
+                }
+                else if (comand[0] == "decrease")
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        array[i] -= 1;
+                    }
+                }
+
+                comand = Console.ReadLine().Split(" ");
+            }
+
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
